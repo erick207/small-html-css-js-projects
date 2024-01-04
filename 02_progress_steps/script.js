8aa0fa74ce32eb7bf4d2a15170487bcf11092935
Erick <12631133+erick207@users.noreply.github.com>
2024-01-04 15:42:33 -0300

    js - progress steps

---
+++ 02_progress_steps/script.js
@@ -0,0 +1,55 @@
+progress = document.getElementById("progress")
+circles = document.querySelectorAll(".circle")
+prev = document.getElementById("prev")
+next = document.getElementById("next")
+
+
+let currentActive = 1
+
+next.addEventListener("click", () => {
+    currentActive++
+    
+    if(currentActive > circles.length) {
+        currentActive = circles.length
+    }
+
+    // Update the DOM
+    update()
+})
+
+prev.addEventListener("click", () => {
+    currentActive--
+
+    if(currentActive < 1) {
+        currentActive = 1
+    }
+
+    // Update the DOM
+    update()
+})
+
+
+function update() {
+    circles.forEach((circle, idx) => {
+        if(idx < currentActive) {
+            circle.classList.add('active')
+        } else {
+            circle.classList.remove('remove')
+        }
+    });
+}
+
+const actives = document.querySelectorAll(".active")
+
+progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + '%'
+
+if(currentActive === 1) {
+    prev.disabled = true
+} else if(currentActive === circles.length) {
+    next.disabled = true
+} else {
+    prev.disabled = false;
+    next.disabled = false;
+}
+
+console.log(currentActive)
