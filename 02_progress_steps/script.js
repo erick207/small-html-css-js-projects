39b28073596ad3871169120d739057a057c63810
Erick <12631133+erick207@users.noreply.github.com>
2024-01-04 16:10:46 -0300

    fixed progress bar

--- 02_progress_steps/script.js
+++ 02_progress_steps/script.js
@@ -1,7 +1,8 @@
 const progress = document.getElementById("progress")
-const  circles = document.querySelectorAll(".circle")
-const  prev = document.getElementById("prev")
+const prev = document.getElementById("prev")
 const next = document.getElementById("next")
+const  circles = document.querySelectorAll(".circle")
+
 
 
 let currentActive = 1
@@ -36,18 +37,19 @@ function update() {
         } else {
             circle.classList.remove('active')
         }
-    });
-}
+    })
 
-const actives = document.querySelectorAll(".active")
 
-progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + '%'
+    const actives = document.querySelectorAll(".active")
 
-if(currentActive === 1) {
-    prev.disabled = true
-} else if(currentActive === circles.length) {
-    next.disabled = true
-} else {
-    prev.disabled = false;
-    next.disabled = false;
+    progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + '%'
+
+    if(currentActive === 1) {
+        prev.disabled = true
+    } else if(currentActive === circles.length) {
+        next.disabled = true
+    } else {
+        prev.disabled = false;
+        next.disabled = false;
+    }
 }
