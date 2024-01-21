be70ef0e3264c11a468bf7bf59a22be9f13797b2
Erick <12631133+erick207@users.noreply.github.com>
2024-01-21 17:02:40 -0300

    add FAQ Collapse project

---
+++ 12_faq_collapse/script.js
@@ -0,0 +1,8 @@
+//const questions = document.querySelectorAll('faq')
+const buttons = document.querySelectorAll('.faq-toggle')
+
+buttons.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        btn.parentNode.classList.toggle('active')
+    })
+})
