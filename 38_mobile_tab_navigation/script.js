7007d8c51cd4e8a62d0c518a92d5b2dcab3f3b78
Erick <12631133+erick207@users.noreply.github.com>
2024-02-22 12:18:52 -0300

    add Mobile Tab Navigation Project

---
+++ 38_mobile_tab_navigation/script.js
@@ -0,0 +1,18 @@
+const contentEls = document.querySelectorAll('.content')
+const liEls = document.querySelectorAll('li')
+
+liEls.forEach((liEl, idx) => 
+    liEl.addEventListener('click', () => {
+        if(!liEl.classList.contains('active')) {
+            findRemoveAndAdd(liEls, idx, 'active')
+            findRemoveAndAdd(contentEls, idx, 'show')
+        }
+    })
+)
+
+const findRemoveAndAdd = (els, idx, className) => {
+    els.forEach(el => {
+        el.classList.remove(className)
+    })
+    els[idx].classList.add(className)
+}
